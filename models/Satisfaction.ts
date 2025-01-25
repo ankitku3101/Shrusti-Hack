af90d03cd98969ffc9c77a8a476e848a10d96c7b
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-01-26 01:21:53 +0530

    Satisfaction Schema defined.

---
+++ models/Satisfaction.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const SatisfactionSchema = new mongoose.Schema(
+    {
+        ofschool:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"School"
+        },
+        clarity:{
+            type:Number,
+            min:[0,"Number can not be negative"],
+            max:[10,"Number can not exceed 10"],
+        },
+        resses:{
+            type:Number,
+            min:[0,"Number can not be negative"],
+            max:[10,"Number can not exceed 10"],
+        },
+        evaluation:{
+            type:Number,
+            min:[0,"Number can not be negative"],
+            max:[10,"Number can not exceed 10"],
+        },
+        studymaterial:{
+            type:Number,
+            min:[0,"Number can not be negative"],
+            max:[10,"Number can not exceed 10"],
+        },
+        safety:{
+            type:Number,
+            min:[0,"Number can not be negative"],
+            max:[10,"Number can not exceed 10"],
+        },
+        extracurricular:{
+            type:Number,
+            min:[0,"Number can not be negative"],
+            max:[10,"Number can not exceed 10"],
+        },
+        environment:{
+            type:String,
+            enum:['Friendly',"Average",'Depressive','hostile']
+        },
+    },
+    {
+        timestamps:true
+    }
+)
+
+export default mongoose.models.Satisfaction || mongoose.model('Satisfaction',SatisfactionSchema)
